188f898a947b7915f6c3449b16391e9249d98be6
User_2 <User_2@lok303>
2014-04-29 16:31:47 +0200

    oefeningen dinsdagmiddag

--- oefening75.php
+++ oefening75.php
@@ -1,9 +1,19 @@
 <?php
-session_start()
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+if ($_GET["reset"]==1) unset ($_SESSION["deur"]);
+if (!isset($_SESSION["deur"])){
+    $_SESSION["deur"]=array();
+    for ($i=1;$i<=7; $i++)$_SESSION["deur"][$i]=0;
+    $_SESSION["schattendeur"]=  rand(1, 7);
+}
+if (isset($_GET["keuze"])){
+    $keuzenr = $_GET["keuze"];
+    if ($keuzenr == $_SESSION["schattendeur"]){
+        $_SESSION["deur"]["keuzenr"]=2;
+    }else{
+        $_SESSION["deur"]["keuzenr"]=1;
+    }
+}
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -19,8 +29,18 @@ session_start()
     <body>
         <h1>Kies een deur</h1>
         <?php for ($i=1; $i <=7; $i++){ ?>
-        <a href="oefening75.php"><img class="deur<?php print $i;?>" src="images/doorclosed.png"></a>
+        <a href="oefening75.php?kiesdeur=<?php print($i);?>">
+        <?php
+        if ($_SESSION["deur"][$i]==0){ ?>
+           <img src="images/doorclosed.png">
+        <?php } elseif ($_SESSION["deur"][$i]==1){?>
+            <img src="images/dooropen.png">
+        <?php } elseif ($_SESSION["deur"][$i]==2){?>
+            <img src="images/doortreasure.png">
+            <?php } ?>
+        </a>
         <?php } ?>
-            <p>Klik <a href="oefening75.php">hier</a> om een nieuw spel te beginnen.</p>
+        <p>Klik <a href="oefening75.php?reset=1">hier</a> om een nieuw spel te spelen.
+       
     </body>
 </html>
