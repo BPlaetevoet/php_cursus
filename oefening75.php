0c535203f37b2d9b56c0a61ddfc0051b86c4a91d
User_2 <User_2@lok303>
2014-04-30 10:06:24 +0200

    Oefening 7.5 met uitbreiding

--- oefening75.php
+++ oefening75.php
@@ -1,17 +1,24 @@
 <?php
 session_start();
-if ($_GET["reset"]==1) unset ($_SESSION["deur"]);
+if (isset($_GET["reset"])){
+    if ($_GET["reset"]==1) unset ($_SESSION["deur"], $_SESSION["teller"]);
+}
+if (!isset ($_SESSION["teller"])){
+    $_SESSION["teller"]=0;
+}
 if (!isset($_SESSION["deur"])){
     $_SESSION["deur"]=array();
     for ($i=1;$i<=7; $i++)$_SESSION["deur"][$i]=0;
     $_SESSION["schattendeur"]=  rand(1, 7);
-}
+   }
+
 if (isset($_GET["keuze"])){
+    $_SESSION["teller"]+=1;
     $keuzenr = $_GET["keuze"];
     if ($keuzenr == $_SESSION["schattendeur"]){
-        $_SESSION["deur"]["keuzenr"]=2;
+        $_SESSION["deur"][$keuzenr]=2;
     }else{
-        $_SESSION["deur"]["keuzenr"]=1;
+        $_SESSION["deur"][$keuzenr]=1;
     }
 }
 ?>
@@ -21,25 +28,32 @@ if (isset($_GET["keuze"])){
         <meta charset="utf-8">
         <title>deurspelletje</title>
         <script type="text/css">
-        img {
-            float:left;           
-        }
+
         </script>
     </head>
     <body>
         <h1>Kies een deur</h1>
-        <?php for ($i=1; $i <=7; $i++){ ?>
-        <a href="oefening75.php?kiesdeur=<?php print($i);?>">
-        <?php
+        <?php for ($i=1; $i <=7; $i++){ 
+            if (!isset($_GET["keuze"]) || ($_SESSION["schattendeur"] !=$_GET["keuze"]) ){?>
+                <a href="oefening75.php?keuze=<?php print($i);?>">
+           <?php }
+         
         if ($_SESSION["deur"][$i]==0){ ?>
            <img src="images/doorclosed.png">
         <?php } elseif ($_SESSION["deur"][$i]==1){?>
             <img src="images/dooropen.png">
         <?php } elseif ($_SESSION["deur"][$i]==2){?>
             <img src="images/doortreasure.png">
-            <?php } ?>
-        </a>
+            <?php } if (!isset($_GET["keuze"]) || ($_SESSION["schattendeur"] != $_GET["keuze"])){
+                print '</a>';}?>
+       
         <?php } ?>
+       <?php if (isset($_GET["keuze"])){
+           if ($_SESSION["schattendeur"]==$_GET["keuze"]){?>
+        <p>Proficiat u had <?php print $_SESSION["teller"];?> pogingen nodig </p>
+        <?php } 
+       }
+        ?>
         <p>Klik <a href="oefening75.php?reset=1">hier</a> om een nieuw spel te spelen.
        
     </body>
