e3c2850956c322e277059b2e81bfff2c60b8319d
User_2 <User_2@lok303>
2014-05-05 11:11:41 +0200

    Oefening 9.2 filmlijst met formulier

--- oefening92.php
+++ oefening92.php
@@ -1,23 +1,70 @@
 <?php
-class FilmList {
-    private $titel;
-    private $duurtijd;
-    static function connectDB(){
-        $con=new PDO("mysql:host=localhost;dbname=cursusphp", "cursusgebruiker", "cursuspwd");
-    }
 
-    public function getFilmLijst(){
+class FilmList {
+
+
+    public function getFilmLijst (){
         $filmlijst =array();
-        $con =self::connectDB();
+        $con = new PDO("mysql:host=localhost;dbname=cursusphp", "cursusgebruiker", "cursuspwd");
         $lijst=($con->query("select titel, duurtijd from films"));
-        return $lijst;        
+        foreach ($lijst as $rij){
+            $film= $rij["titel"]." (".$rij["duurtijd"].")";
+            array_push($filmlijst, $film);
+        }
+        $con=null;
+        return $filmlijst;
+        
     }
-    public function FilmToevoegen($t, $d){
-        $con=self::connectDB();
-        $sql="insert into films titel, duurtijd";
+    public function controleerFilm($titel, $duur){
+        if (!empty($titel) && is_numeric($duur) && $duur>0){
+            $checkfilm = true;
+            return $checkfilm;
+        }
+    }
+    public function FilmToevoegen($titel, $duur){
+        $con = new PDO("mysql:host=localhost;dbname=cursusphp", "cursusgebruiker", "cursuspwd");
+        $sql="insert into films (titel, duurtijd) values ('".$titel."',".$duur.")";
         $con->exec($sql);
         $con = null;  
     }
 }
+$toevoegen = new FilmList();
+if (isset($_GET["actie"])&& $_GET["actie"]=="toevoegen"){
+    if ($toevoegen->controleerFilm($_POST["titel"], $_POST["duurtijd"])) {
+        
+        $toevoegen->FilmToevoegen($_POST["titel"], $_POST["duurtijd"]);
+    }
+    else {
+        print 'toevoegen mislukt';
+    }
+    
+}
 ?>
 <!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>Oefening 9.2</title>
+    </head>
+    <body>
+        <h1>Films</h1>
+        <ul>
+            <?php
+            $data= new FilmList();
+            $lijst = ($data->getFilmLijst());
+            foreach ($lijst as $rij){
+                echo '<li>'.$rij.'</li>';
+            }?>
+        </ul>
+        <p>&nbsp;</p>
+        <h1>Film toevoegen</h1>
+        <form action="oefening92.php?actie=toevoegen" method="post">
+            Titel: 
+            <input type='text' name='titel'><br />
+            Duurtijd :
+            <input type="text" name="duurtijd"> minuten<br />
+            <input type="submit" value="Verzenden">
+        </form>
+        
+    </body>
+</html>
